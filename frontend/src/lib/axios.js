fc4d1b19df946b544bb7769a369a0ef40489dd06
jainjhalak <jainjhalak9491@gmail.com>
2025-12-14 23:19:05 +0530

    Project completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,4 +3,5 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api", //backend url
     withCredentials: true,
-});
+}
+)
